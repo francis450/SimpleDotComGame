5287c0308e84863c73bd427f2044566de212968f
francis kamande <franciskamande2001@gmail.com>
2022-04-17 05:12:35 +0300

    takes in message to be displayed when propting user input, it returns the results of a guess including miss, kill or hit

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -5,4 +5,24 @@ class SimpleDotCom{
 	public void setLocation(int [] loc){
 		locationCells = loc;
 	}
+
+	public String checkYourself(String stringGuess){
+		int guess = Integer.parseInt(stringGuess);
+		String result ="miss";
+
+		for(int cell : locationCells){
+			if(guess  == cell){
+				result = "hit";
+				numOfHits++;
+				//Since we already got a hit there is no need to test the other classes
+				break;
+			}
+		}
+		if(numOfHits  == locationCells.length){
+			result = "kill";
+		}
+
+		System.out.println(result);
+		return result; 
+	}
 }
